bceae5075b4486f3520f5e8edf79f869668f1bdb
doctype-Melvin <doctype.melvin@gmail.com>
2023-01-24 11:46:06 +0100

    adds layout and browser router

---
+++ src/routes/root.jsx
@@ -0,0 +1,10 @@
+export default function Root() {
+    return (
+        <div id="main">
+            <div id="header">Header</div>
+            <div id="sidebar">Sidebar</div>
+            <div id="content">Content</div>
+            <div id="footer">footer</div>
+        </div>
+    )
+}
